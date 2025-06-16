5839d96a2cee49d9871d79d2caee5f99a93fc236
Arman Hossain <armanmahim00@gmail.com>
2025-06-16 15:02:10 +0600

    DASHBOARD UPDATE

--- src/main.jsx
+++ src/main.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'Dashboard',
-        element: <CartList></CartList>
+        element: <Dashboard></Dashboard>
       },
     ]
   }
